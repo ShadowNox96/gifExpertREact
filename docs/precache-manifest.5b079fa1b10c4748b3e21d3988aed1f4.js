732a59400f333bd353c255521851dd6f33957c60
ShadowNox96 <cdcarlose32@gmail.com>
2020-07-17 12:37:56 -0600

    Segundo Commit

---
+++ docs/precache-manifest.5b079fa1b10c4748b3e21d3988aed1f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c577115d88e34895ea851daa9f8c0c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4de40c8475242cd97b2",
+    "url": "/static/css/main.d500ef57.chunk.css"
+  },
+  {
+    "revision": "e04b4afaac7a7f0f7a3b",
+    "url": "/static/js/2.7766e5a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7766e5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4de40c8475242cd97b2",
+    "url": "/static/js/main.92ade706.chunk.js"
+  },
+  {
+    "revision": "2e60dea91f8c043f688d",
+    "url": "/static/js/runtime-main.985cb9c8.js"
+  }
+]);
